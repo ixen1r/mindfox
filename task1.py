2f90131156cffcecbff6e70a013058e1aa71523a
ixen1r <153447607+ixen1r@users.noreply.github.com>
2024-02-20 01:55:34 +0700

    task1.py

--- task1.py
+++ task1.py
@@ -1,34 +1,25 @@
-def res(t):
-    rever = t[::-1]
-    return rever
-
-def reverse_words(text): 
-    sl = {}
+def reverse_words(text : str)-> str: 
     reversed_text = ""
-    for char in text:
+    for char in text.split():
+        sl = {}
         if char.isalpha():
-            reversed_text += f'{res(char)} '
+            reversed_text += f'{char[::-1]} '
         else:
             slovo = char
             for i,num in enumerate(char):
                 if not char[i].isalpha():
                     sl[i] = num
-                    slovo = slovo.replace(sl[i], '')
-                    
-            slovo = res(slovo)
-            
+                    slovo = slovo.replace(sl[i], '')       
+            slovo = slovo[::-1]
             for i in sl:
                 slovo = slovo[:i] + sl[i] + slovo[i::]
             reversed_text += f'{slovo} '
-            
-    return reversed_text
-
-if __name__ == '__main___':
-    cases = [
-        ("abcd efgh", "dcba hgfe"), 
-        ("a1bcd efg!h", "d1cba hgf!e"),
-        ("", ""),
-]
-    for text, reversed_text in cases:
-        assert reverse_words(text) == reversed_text
-        
+    return reversed_text.strip()
+cases = [
+    ("abcd efgh", "dcba hgfe"), 
+    ("a1bcd efg!h", "d1cba hgf!e"),
+    ("q2wert qw 11qq", "t2rewq wq 11qq"),
+    ("", ""),]
+for text, reversed_text in cases:
+    assert reverse_words(text) == reversed_text
+  
